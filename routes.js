2b8ed117d222b866c45d6a6a769ce92c861a661f
B-AlexiaYoung <brlxyng@gmail.com>
2018-10-07 13:26:58 -0600

    new patron validation, pagination and search

--- routes.js
+++ routes.js
@@ -13,23 +13,9 @@ let Patrons = require("./models").patrons;
 router.get("/", (req, res) => {
     res.render('index');
 });
-// routing all books
-router.get("/books", (req, res) => {
-    Books.findAll().then(function (books) {
-        //console.log(books);
-        res.render("all_books", {
-            bookinfo: books,
-            pagetitle: "All Books"
-        });
-    })
-
-});
-
-// // routing all books  with pagination   just starting to work on this
+// // routing all books
 // router.get("/books", (req, res) => {
-//     let limit= 5;
-//     let offset= 0;
-//     Books.findAndCountAll().then(function (books) {
+//     Books.findAll().then(function (books) {
 //         //console.log(books);
 //         res.render("all_books", {
 //             bookinfo: books,
@@ -37,7 +23,81 @@ router.get("/books", (req, res) => {
 //         });
 //     })
 
-});
+// });
+
+// //routing to allbooks redirect
+// router.get("/books", (req, res) => {
+// res.redirect("/books/1")
+// });
+// // // routing all books  with pagination   just starting to work on this
+// //router.get("/books", (req, res) => {
+// router.get("/books/:page", (req, res, next) => {
+//     let page= req.params.page;
+
+//     let limit= 5;
+//     Books.findAndCountAll({
+
+//         limit:limit,
+//         offset:limit * ( page -1),
+//     })
+//         .then((books)=>{
+//             let paginate = Math.ceil(books.count / limit);
+//         console.log(books.count);
+//         console.log(page);
+//         console.log(paginate);
+//         console.log(books.rows);
+//             res.render("all_books", {
+//                 bookinfo: books.rows,
+//                 pagetitle: "All Books",
+//                 paginate: paginate,
+//                 count : books.count
+//             })  
+//     })
+// });
+
+//routing searchbooks
+router.post("/searchbooks", (req,res,next)=>{
+    console.log(req.body.search);
+    let limit= 5
+    let page = 1
+    Books.findAndCountAll({
+        
+        limit:limit,
+        offset:limit * ( page -1),
+        
+        where:{
+            [Op.or]:{
+                title:{
+                [Op.like]: '%'+ req.body.search +'%',
+                },
+                author:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                    },
+                genre:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                    },
+                first_published:{
+                    [Op.eq]:  req.body.search,
+                    },
+                id:{
+                    [Op.eq]:  req.body.search,
+                    }
+            } 
+        }
+    }).then((books)=>{
+        let paginate = Math.ceil(books.count / limit);
+        // console.log(books.count);
+        // console.log(page);
+        // console.log(paginate);
+        // console.log(books.rows);
+        res.render("all_books", {
+            bookinfo: books.rows,
+            pagetitle: "All Books",
+            paginate: paginate,
+            count : books.count
+        })  
+    })
+})
 
 // routing overdue books
 router.get("/books/overdue", (req, res) => {
@@ -146,16 +206,50 @@ router.get("/new_loan", (req, res) => {
         })
 });
 
-//routing display all patrons
+
+//routing all patrons redirect
 router.get("/patrons", (req, res) => {
-    Patrons.findAll().then(function (patrons) {
-        //console.log(patrons);
-        res.render("all_patrons", {
-            patroninfo: patrons,
-            pagetitle: "All Patrons"
-        });
+    res.redirect("/patrons/1")
+})
+//routing display all patrons with pagination
+router.get("/patrons/:page", (req, res) => {
+    let page= req.params.page;
+
+    let limit= 5;
+    Patrons.findAndCountAll({
+
+        limit:limit,
+        offset:limit * ( page -1),
     })
-});
+        .then((patrons)=>{
+            let paginate = Math.ceil(patrons.count / limit);
+        // console.log(patrons.count);
+        // console.log(page);
+        // console.log(paginate);
+        // console.log(patrons.rows);
+            res.render("all_patrons", {
+                patroninfo: patrons.rows,
+                pagetitle: "All Patrons",
+                paginate: paginate,
+                count : patrons.count
+            })  
+    })
+})
+
+
+
+
+
+//routing display all patrons
+// router.get("/patrons", (req, res) => {
+//     Patrons.findAll().then(function (patrons) {
+//         //console.log(patrons);
+//         res.render("all_patrons", {
+//             patroninfo: patrons,
+//             pagetitle: "All Patrons"
+//         });
+//     })
+// });
 //routing return book
 router.get("/return/:book_id", (req, res) => {
     Loans.findOne({
@@ -200,7 +294,7 @@ router.post("/books/create_new_book", function (req, res, next) {
             res.redirect('/books')
         }).catch(function (error) {
             if (error.name === "SequelizeValidationError") {
-                console.log(Books.build(req.body));
+                //console.log(Books.build(req.body));
 
 
                 res.render('books_new', {
@@ -222,15 +316,78 @@ router.post("/books/create_new_book", function (req, res, next) {
 router.post("/patrons/create_new_patron", function (req, res, next) {
     //console.log(req.body);
     Patrons.create(req.body)
-        .then(res.redirect('/patrons'))
+        .then(function(patrons){
+            res.redirect('/patrons')
+        })
         .catch(function (error) {
             if (error.name === "SequelizeValidationError") {
+                res.render('new_patron', {
+                    book: Patrons.build(req.body),
+
+                    errors: error.errors
+                });
 
             }//end if
+        }).catch(function (error) {
+            console.log(error);
         })
 
 })
 
+
+//routing searchpatrons
+router.post("/searchpatrons", function (req,res, next){
+    let page= 1;
+
+    let limit= 5;
+    Patrons.findAndCountAll({
+
+        limit:limit,
+        offset:limit * ( page -1),
+        where:{
+            [Op.or]:{
+                first_name:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                },
+                last_name:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                    },
+                address:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                    },
+                email:{
+                     [Op.like]: '%'+ req.body.search +'%',
+                    },
+                library_id:{
+                    [Op.like]: '%'+ req.body.search +'%',
+                    },
+                zip_code:{
+                    [Op.eq]: req.body.search,
+                    },
+                id:{
+                    [Op.eq]:  req.body.search,
+                    }
+            } 
+        }
+    })
+        .then((patrons)=>{
+            let paginate = Math.ceil(patrons.count / limit);
+        // console.log(patrons.count);
+        // console.log(page);
+        // console.log(paginate);
+        // console.log(patrons.rows);
+            res.render("all_patrons", {
+                patroninfo: patrons.rows,
+                pagetitle: "All Patrons",
+                paginate: paginate,
+                count : patrons.count
+            })  
+    })
+})
+//routing to allbooks redirect
+router.get("/books", (req, res) => {
+    res.redirect("/books/1")
+    });
 
 // routing individual book - books_detail
 // router.get("/books/:id", (req, res) => {
@@ -269,8 +426,35 @@ router.post("/patrons/create_new_patron", function (req, res, next) {
 //         })
 // });
 
-//routing individual book, book-details
-router.get("/books/:id", (req, res) => {
+
+
+    // // routing all books  with pagination   just starting to work on this
+    //router.get("/books", (req, res) => {
+    router.get("/books/:page", (req, res, next) => {
+        let page= req.params.page;
+    
+        let limit= 5;
+        Books.findAndCountAll({
+    
+            limit:limit,
+            offset:limit * ( page -1),
+        })
+            .then((books)=>{
+                let paginate = Math.ceil(books.count / limit);
+            // console.log(books.count);
+            // console.log(page);
+            // console.log(paginate);
+            // console.log(books.rows);
+                res.render("all_books", {
+                    bookinfo: books.rows,
+                    pagetitle: "All Books",
+                    paginate: paginate,
+                    count : books.count
+                })  
+        })
+    });
+ //routing individual book, book-details
+router.get("/books/details/:id", (req, res) => {
     //console.log(req.params.id)
     let bk = req.params.id;
     Books.findOne({
@@ -307,8 +491,7 @@ router.get("/books/:id", (req, res) => {
             //res.render("book_details", {bKloan: books});
         })
     })
-});
-
+});   
 
 //routing update 'put' to update individual book information 
 router.put("/books/update/:id", (req, res, next) => {
@@ -388,12 +571,17 @@ router.get("/books/patrons/patron_details/:id", (req, res, next) => {
             id: req.params.id
         },
         }).then(function (patrons){
+            
             Loans.findAll({
+                include: {
+                    model: Books
+                },
+
                 where:{
                     patron_id: singlePatron
                 }
             }).then(function (loans) {
-            //  console.log(loans);
+             //console.log(loans[0].book.title);
             //console.log(patrons.dataValues)
             //console.log(patrons.dataValues.loans[0].dataValues.book_id);
             //let loanDetails = patrons.dataValues.loans[0].dataValues
@@ -433,7 +621,7 @@ router.put("/books/patrons/patron_details/:id", (req, res, next) => {
             id: req.params.id
         }
     }).then(function (patrons) {
-        console.log(patrons.id)
+        //console.log(patrons.id)
          patronId =patrons.id;
         return patrons.update(req.body);
         
@@ -448,8 +636,8 @@ router.put("/books/patrons/patron_details/:id", (req, res, next) => {
                 }
             
             }).then(function (loans) {
-                    console.log("wibble");
-                    console.log(loans);
+                    // console.log("wibble");
+                    // console.log(loans);
                     // console.log(Patrons.build(req.body));
                     let customerUpdate = Patrons.build(req.body);
                     let customerError = error.errors
@@ -492,7 +680,7 @@ router.put("/return/returned/:book_id", (req, res, next) => {
                     book_id: req.params.book_id,
                 }
             }).then(function (loans) {
-                console.log(req.body);
+                //console.log(req.body);
                 res.render('return', {
                     loanreturn: loans,
                     errors: error.errors,
@@ -511,7 +699,7 @@ router.put("/return/returned/:book_id", (req, res, next) => {
 
 // routing update loans table for new loan
 router.post("/loans/new_loan_update", (req, res, next) => {
-    console.log(req.body);
+    //console.log(req.body);
     let checkLoan= req.body;
     checkLoan.returned_on = null;
     Loans.create(checkLoan)
@@ -530,8 +718,8 @@ router.post("/loans/new_loan_update", (req, res, next) => {
                         },
                     })
                     .then(function (books) {
-                        console.log(noDate);
-                        console.log(books[0]);
+                        // console.log(noDate);
+                        // console.log(books[0]);
                         Patrons.findAll()
                             .then(function (patrons) {
                                 res.render("new_loan", {
